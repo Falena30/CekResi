d63ac2951d9ee8352810f2d05e7aaac0d510fb46
Falena30 <dasuyikno@gmail.com>
2021-05-06 12:35:43 +0700

    done with details

--- app/Http/Controllers/cekResi.php
+++ app/Http/Controllers/cekResi.php
@@ -12,7 +12,7 @@ class cekResi extends Controller
     public function index(Request $request)
     {
         $jsondata = CekResiModels::getApiCekResi('pos','18022470553');
-         //dd($response);
-        return view('cekResi.index', ['summarys'=>$jsondata['data']['summary'],'details'=>$jsondata['data']['detail'],'historys'=>$jsondata['data']['history']]);
+        $sortedArr = collect($jsondata['data']['history'])->sortBy('date')->all();
+        return view('cekResi.index', ['summarys'=>$jsondata['data']['summary'],'details'=>$jsondata['data']['detail'],'historys'=>$sortedArr]);
     }
 }
