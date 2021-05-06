f3e4018cc53495920344542a28e853b8bd266f88
Falena30 <dasuyikno@gmail.com>
2021-05-06 12:25:18 +0700

    done with stats

--- app/Http/Controllers/cekResi.php
+++ app/Http/Controllers/cekResi.php
@@ -12,7 +12,6 @@ class cekResi extends Controller
     public function index(Request $request)
     {
         $jsondata = CekResiModels::getApiCekResi('pos','18022470553');
-
          //dd($response);
         return view('cekResi.index', ['summarys'=>$jsondata['data']['summary'],'details'=>$jsondata['data']['detail'],'historys'=>$jsondata['data']['history']]);
     }
--- resources/views/cekResi/index.blade.php
+++ resources/views/cekResi/index.blade.php
@@ -30,25 +30,60 @@
 
 @section('detail')
     <div class="col-md-8 col-sm-12 bg-white p-4">
-      <table class="table">
+      <table class="table table-sm">
         <thead>
             <tr>
-              <th>Delivered to {{$details['receiver']}}, {{$details['origin']}},{{$historys[0]['date']}}</th>
+              <th class="text-center" colspan="4">Delivered to {{$details['receiver']}}, {{$details['destination']}}, {{$historys[0]['date']}}</th>
             </tr>
         </thead>
         <tbody>
             <tr>
                 <th>Nomer Resi</th>
+                <th>Tanggal Pengiriman</th>
+                <th>Pengirim</th>
+                <th>Penerima</th>
+            </tr>
+            <tr>
                 <td>{{$summarys['awb']}}</td>
+                <td>{{$summarys['date']}}</td>
+                <td>{{$details['shipper']}}</td>
+                <td>{{$details['receiver']}}</td>
             </tr>
             <tr>
-                <th>Kurir</th>
-                <td>{{$summarys['courier']}}</td>
+              @if ($summarys['courier']!="")
+                <td colspan="2">Courier Terpilih</td>
+              @else
+                <td colspan="2">Courier tidak terpilih</td>
+              @endif
+                <td>{{$details['origin']}}</td>
+                <td>{{$details['destination']}}</td>
             </tr>
             <tr>
-                <th>Layanan</th>
-                <td>{{$summarys['service']}}</td>
+              @if ($summarys['service']!="")
+                <td colspan="4">Service Terpilih</td>
+              @else
+                <td colspan="4">Service tidak terpilih</td>
+              @endif
             </tr>
+        </tbody>
+    </table>
+    <table class="table table-sm">
+        <thead>
+            <tr>
+              <th colspan="2">Detail Pengiriman</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <th>Tanggal/Waktu</th>
+              <th>Deskribsi</th>
+            </tr>
+            @foreach ($historys as $history)
+            <tr>
+              <td>{{$history['date']}}</td>
+              <td>{{$history['desc']}}</td>
+            </tr>
+            @endforeach
         </tbody>
     </table>
     </div>
